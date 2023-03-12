d9f0a7849e295746d57ddc0603741aabdda90e49
Enzo C <enzo20112002@gmail.com>
2023-03-12 22:24:13 +0100

    Edit ajouterEffet in banqueQuestions

--- src/main/banqueQuestion.java
+++ src/main/banqueQuestion.java
@@ -10,6 +10,7 @@ public class banqueQuestion {
 
 
     public banqueQuestion() {
+        this.questions = new ArrayList<>();
     }
 
     public ArrayList<Question> getQuestions() {
@@ -31,49 +32,49 @@ public class banqueQuestion {
         Question question1 = new Question(
                 "Main du roi",
                 "Le peuple souhaite libérer les prisonniers",
-                "Oui",
-                "Non");
-        question1.ajouteEffetGauche(TypeJauge.ARMEE, -5);
-        question1.ajouteEffetGauche(TypeJauge.PEUPLE, +5);
-        question1.ajouteEffetDroite(TypeJauge.PEUPLE, -7);
+                new Effets("Oui", TypeEffet.G),
+                new Effets("Non", TypeEffet.D));
+        question1.EffetGauche.ajouterEffets(TypeJauge.ARMEE, -5);
+        question1.EffetGauche.ajouterEffets(TypeJauge.PEUPLE, +5);
+        question1.EffetDroit.ajouterEffets(TypeJauge.PEUPLE, -7);
         questions.add(question1);
         Question question2 = new Question(
                 "Paysan",
                 "Il n'y a plus rien à manger",
-                "Importer de la nourriture",
-                "Ne rien faire");
-        question2.ajouteEffetGauche(TypeJauge.FINANCE,-5);
-        question2.ajouteEffetGauche(TypeJauge.PEUPLE, +5);
-        question2.ajouteEffetDroite(TypeJauge.PEUPLE, -5);
+                new Effets("Importer de la nourriture", TypeEffet.G),
+                new Effets("Ne rien faire", TypeEffet.D));
+        question2.EffetGauche.ajouterEffets(TypeJauge.FINANCE,-5);
+        question2.EffetGauche.ajouterEffets(TypeJauge.PEUPLE, +5);
+        question2.EffetDroit.ajouterEffets(TypeJauge.PEUPLE, -5);
         questions.add(question2);
         Question question3 = new Question(
                 "Prêtre",
                 "Les dieux sont en colère",
-                "Faire un sacrifice",
-                "Ne rien faire");
-        question3.ajouteEffetGauche(TypeJauge.CLERGE, +5);
-        question3.ajouteEffetGauche(TypeJauge.PEUPLE, -3);
-        question3.ajouteEffetDroite(TypeJauge.CLERGE, -5);
+                new Effets("Faire un sacrifice", TypeEffet.G),
+                new Effets("Ne rien faire", TypeEffet.D));
+        question3.EffetGauche.ajouterEffets(TypeJauge.CLERGE, +5);
+        question3.EffetGauche.ajouterEffets(TypeJauge.PEUPLE, -3);
+        question3.EffetDroit.ajouterEffets(TypeJauge.CLERGE, -5);
         questions.add(question3);
         Question question4 = new Question(
                 "Main du roi",
                 "Le roi Baratheon rassemble son armée",
-                "Le soutenir",
-                "Rester neutre");
-        question4.ajouteEffetGauche(TypeJauge.ARMEE, +3);
-        question4.ajouteEffetGauche(TypeJauge.FINANCE, -3);
-        question4.ajouteEffetGauche(TypeJauge.CLERGE, -3);
-        question4.ajouteEffetDroite(TypeJauge.PEUPLE, +3);
+                new Effets("Le soutenir", TypeEffet.G),
+                new Effets("Rester neutre", TypeEffet.D));
+        question4.EffetGauche.ajouterEffets(TypeJauge.ARMEE, +3);
+        question4.EffetGauche.ajouterEffets(TypeJauge.FINANCE, -3);
+        question4.EffetGauche.ajouterEffets(TypeJauge.CLERGE, -3);
+        question4.EffetDroit.ajouterEffets(TypeJauge.PEUPLE, +3);
         questions.add(question4);
         Question question5 = new Question(
                 "Paysan",
                 "Abondance de récoltes cette année",
-                "Taxer énormément",
-                "Taxer un tout petit peu");
-        question5.ajouteEffetGauche(TypeJauge.FINANCE, +10);
-        question5.ajouteEffetGauche(TypeJauge.PEUPLE, -5);
-        question5.ajouteEffetDroite(TypeJauge.FINANCE, +1);
-        question5.ajouteEffetDroite(TypeJauge.PEUPLE, -3);
+                new Effets("Taxer énormément", TypeEffet.G),
+                new Effets("Taxer un tout petit peu", TypeEffet.D));
+        question5.EffetGauche.ajouterEffets(TypeJauge.FINANCE, +10);
+        question5.EffetGauche.ajouterEffets(TypeJauge.PEUPLE, -5);
+        question5.EffetDroit.ajouterEffets(TypeJauge.FINANCE, +1);
+        question5.EffetDroit.ajouterEffets(TypeJauge.PEUPLE, -3);
         questions.add(question5);
     }
 
